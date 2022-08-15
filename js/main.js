0ccc8b04f2bab249647c6c5b90e244feb647c1fb
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2022-08-15 09:50:08 -0300

    /update
    add libreries toastify, react js & sweetAlert

--- js/main.js
+++ js/main.js
@@ -38,3 +38,27 @@ localStorage.setItem("alumnos", enJSON);
 
 const alumnosEnTexto = JSON.parse(localStorage.getItem("alumnos"));
 console.log(alumnos.id);
+
+Toastify({
+    text: "No te olvides de ver mi toDoList!",
+    newWindow: true,
+    close: true,
+    gravity: "buttom", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function() {} // Callback after click
+}).showToast();
+
+Toastify({
+    text: "toDoList",
+    duration: 9999999999,
+    destination: "modulo_tareas/tareas.html",
+    className: "info", // `top` or `bottom`
+    position: "buttom",
+    style: {
+        background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+}).showToast();
