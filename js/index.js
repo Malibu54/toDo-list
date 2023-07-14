cc59aeac63152cd351e5ad852a50f98e446cc7df
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2023-07-14 14:56:42 -0300

    //Delete
    Deleted from all commented code

--- js/index.js
+++ js/index.js
@@ -59,39 +59,3 @@ const renderOrderedTasks = () => {
 };
 
 setDate();
-/*
-area.value = localStorage.getItem("area");
-area.oninput = () => {
-  localStorage.setItem("area", area.value);
-};
-
-Swal.fire({
-  title: "Submit your Github username",
-  input: "text",
-  inputAttributes: {
-    autocapitalize: "off",
-  },
-  showCancelButton: true,
-  confirmButtonText: "Look up",
-  showLoaderOnConfirm: true,
-  preConfirm: (login) => {
-    return fetch(`//api.github.com/users/${login}`)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch((error) => {
-        Swal.showValidationMessage(`Request failed: ${error}`);
-      });
-  },
-  allowOutsideClick: () => !Swal.isLoading(),
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire({
-      title: `${result.value.login}'s avatar`,
-      imageUrl: result.value.avatar_url,
-    });
-  }
-});*/
